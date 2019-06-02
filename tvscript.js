8a519e7150ef34b161d675ebc6f04a1fc65124d0
Oleg Galkin <oleg@videoworklink.com>
2019-06-02 16:16:21 +0700

    dbg

--- tvscript.js
+++ tvscript.js
@@ -31,7 +31,8 @@
 									var parsedVideo = parsedVideos[index];
 									var timeWhenAdded = moment(parsedVideo["TimeWhenAdded"], "MM/DD/YYYY hh:mm:ss a").toDate();
 									var videoWithDate = {Title : parsedVideo["Title"], VideoURL : parsedVideo["VideoURL"], TimeWhenAdded : timeWhenAdded, Description : parsedVideo["Description"], 
-									Duration : parsedVideo["Duration"]};									
+									Duration : parsedVideo["Duration"]};
+									console.log(videoWithDate);
 									sortedVideos.push(videoWithDate);
 								}
 								
